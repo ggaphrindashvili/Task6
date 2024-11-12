50bf4b5d4ea8fab80f6a13a53ef18b8f5297823a
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-12 18:00:40 +0400

    Task_3_Gocha

---
+++ src/test/java/Animal.java
@@ -0,0 +1,35 @@
+class Animal {
+    protected String species;
+
+    public Animal(String species) {
+        this.species = species;
+    }
+}
+
+class Mammal extends Animal {
+    protected String diet;
+
+    public Mammal(String species, String diet) {
+        super(species);
+        this.diet = diet;
+    }
+}
+
+class Dog extends Mammal {
+    private String breed;
+
+    public Dog(String species, String diet, String breed) {
+        super(species, diet);
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" + "species='" + species + "', diet='" + diet + "', breed='" + breed + "'}";
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("ცუგრუმელა", "კომპეტენტო", "German shephard");
+        System.out.println(dog);
+    }
+}
