8f688cd5d31402414044f5803068aa520f7df279
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-12 17:57:10 +0400

    Task_2_Gocha

--- src/test/java/Car.java
+++ src/test/java/Car.java
@@ -3,22 +3,26 @@ public class Car {
     private int year;
     private double price;
 
-    public Car(String model, int year, double price) {
-        if (year < 2000 || year > 2024) {
-            System.out.println("Year should be between 2000 and 2024.");
+    public Car(String model) {
+        this(model, 2000, 0);
+    }
 
+    public Car(String model, int year, double price) {
+        this.model = model;
+
+        if (year < 2000 || year > 2024) {
+            System.out.println("Error: Year must be between 2000 and 2024.");
+            this.year = 2000;
         } else {
             this.year = year;
         }
 
         if (price < 0) {
-            System.out.println("Price should not be negative.");
-
+            System.out.println("Error: Price cannot be negative.");
+            this.price = 0;
         } else {
             this.price = price;
         }
-
-        this.model = model;
     }
 
     @Override
@@ -27,8 +31,8 @@ public class Car {
     }
 
     public static void main(String[] args) {
-        Car car1 = new Car("Honda", 2022, 19000);
-        Car car2 = new Car("Ford", 1999, -5000);
+        Car car1 = new Car("Toyota");
+        Car car2 = new Car("Ford", 2015, 18000);
         System.out.println(car1);
         System.out.println(car2);
     }
